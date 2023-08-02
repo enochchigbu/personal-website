8614c7c600df8af9aef6ea1885a016125d9232de
enochchigbu <enochchigbu@gmail.com>
2023-08-01 21:22:48 -0400

    Create Home component, slight refactor to App component

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component{
+    render() {
+        return(
+            <div>
+                <header className="header">
+                    Hello, my name is Enoch
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Home;
