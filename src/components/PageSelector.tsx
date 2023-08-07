b605229510d5dcaaa39e95fdf83d78be1f2dbae1
enochchigbu <enochchigbu@gmail.com>
2023-08-06 23:56:20 -0400

    Added state to pageSelector

--- src/components/PageSelector.tsx
+++ src/components/PageSelector.tsx
@@ -1,10 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './PageSelector.css';
 
-class PageSelector extends Component{
+/**
+ * Page Selector Component
+ * 
+ * This is the component that handles page selection and switching.
+ * 
+ * 4 States ( Name : Function ):
+ *      - Home : The Home page, first page a user sees
+ *      - About : A page containing background info
+ *      - Contact : Page that contains my contact info
+ *      - Skills : Page that displays my technical skiils / projects
+ *          Note: This can be added later after you do more projects
+ */
+
+enum Pages { HOME, ABOUT, CONTACT, SKILLS }
+
+interface ComponentProps{ page : Pages }
+
+class PageSelector extends Component<ComponentProps>{
     render() {
+        const { page } = this.props;
+        
         return(
             <div>
+                <button>
+                    Home
+                </button>
+                <button>
+                    About Me
+                </button>
+                <button>
+                    Contact
+                </button>
+                <button>
+                    Skills/Projects
+                </button>
             </div>
         )
     }
