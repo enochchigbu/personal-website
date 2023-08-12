6af35c054ffa41691818702df64d02f0b2691a67
enochchigbu <enochchigbu@gmail.com>
2023-08-12 17:28:39 -0500

    Created skeleton code for contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './Contact.css';
 
+/**
+ * Contact Page
+ * 
+ * Where the user can contact me
+ */
+
 class Contact extends Component{
     render() {
         return(
-            <div>
-                Contact
+            <div className='container'>
+                <div className='text-area'>
+                    <p>Contact Me</p>
+                </div>
+                <div className='input-area'>
+                    <div className='input-container'>
+                        <header>Name</header>
+                        <textarea className='input-field'/>                        
+                    </div>
+                    <div className='input-container'>
+                        <header>Email</header>
+                        <textarea className='input-field'/>
+                    </div>
+                    <div className='input-container'>
+                        <header>Message</header>
+                        <textarea className='input-field'/>
+                    </div>
+                    <button>
+                        Submit
+                    </button>
+                </div>
             </div>
         )
     }
