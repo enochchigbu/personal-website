a88f3d2b072191338c443d210dc26409e8e418a2
enochchigbu <enochchigbu@gmail.com>
2023-08-12 13:13:27 -0400

    Added Projects.tsx page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,6 @@ import Home from "./Home";
 import Contact from "./Contact";
 import About from "./About";
 import Skills from "./Skills";
+import Projects from "./Projects";
 
-export { Home, About, Skills, Contact };
+export { Home, About, Skills, Contact, Projects };
