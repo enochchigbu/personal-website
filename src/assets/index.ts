68e23ffbf87e23a7b74a6c2fa439268a1a956754
enochchigbu <enochchigbu@gmail.com>
2023-08-14 01:27:57 -0700

    Add image assets for contact page

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import linkedInLogo from "/Users/enochchigbu/Portfolio/personal-website/src/assets/images/linkedin-logo.png";
+import instragramLogo from "/Users/enochchigbu/Portfolio/personal-website/src/assets/images/instagram-logo.png";
+import githubLogo from "/Users/enochchigbu/Portfolio/personal-website/src/assets/images/github-logo.png"
+
+export {linkedInLogo, instragramLogo, githubLogo};
